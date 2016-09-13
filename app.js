a3b3272b4d925b87f78bbf739a1233ffcaef1fa1
Edith Li <abenoy05@gmail.com>
2016-09-13 16:49:06 +0000

    initial

---
+++ app.js
@@ -0,0 +1,35 @@
+var app = require('http').createServer(handler);
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+function handler (req, res) {
+	fs.readFile(__dirname + '/public/index.html',
+		function(err, data){
+			res.writeHead(200);
+			res.end(data);
+		})
+	console.log("user connected");
+}
+
+app.listen(8000);
+
+io.on('connection', function (socket){
+	console.log("user connected to socket");
+
+	socket.on('messageFromClientToServer', function(data){
+		console.log(data);
+	});
+
+	var sendRandomNumbers = setInterval( function(){
+		var numberData = Math.random() * 100;
+		console.log(numberData);
+		socket.emit('messageFromServerToClient', numberData);
+	
+	}, 1000);
+	
+	socket.on('disconnect', function(){
+		console.log("user disconnected from socket");
+		clearInterval(sendRandomNumbers);
+	});
+	
+})
