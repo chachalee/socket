96c0c8a66121d25ac8bb9b1bcfd7f521af382492
Edith Li <Edith Li>
2016-09-14 23:22:44 -0400

    use original code from tutorial

--- app.js
+++ app.js
@@ -4,32 +4,30 @@ var fs = require('fs');
 
 function handler (req, res) {
 	fs.readFile(__dirname + '/public/index.html',
-		function(err, data){
-			res.writeHead(200);
-			res.end(data);
-		})
+	function (err, data) {
+		res.writeHead(200);
+		res.end(data);
+	});
 	console.log("user connected");
 }
 
 app.listen(8000);
 
-io.on('connection', function (socket){
+io.on('connection', function (socket) {
 	console.log("user connected to socket");
-
+	
 	socket.on('messageFromClientToServer', function(data){
 		console.log(data);
 	});
-
+	
 	var sendRandomNumbers = setInterval( function(){
 		var numberData = Math.random() * 100;
 		console.log(numberData);
 		socket.emit('messageFromServerToClient', numberData);
-	
-	}, 1000);
+	},1000);
 	
 	socket.on('disconnect', function(){
 		console.log("user disconnected from socket");
 		clearInterval(sendRandomNumbers);
 	});
-	
-})
+});
